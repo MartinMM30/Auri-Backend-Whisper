8150e299a39a4394fba3f504fe5536ffc8685945
Martin Mendez <martinstward.mmm@gmail.com>
2025-11-28 19:08:40 -0600

    fix backend

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import uvicorn
+from server import app   # importa tu FastAPI principal
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
